b5b820ac63b3433f32c5d0716d9f4cae35487c48
LuxianAiolos <luxianaiolos@gmail.com>
2022-10-15 11:19:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,36 @@ import {useEffect, useState} from 'react'
 
 function App() {
   const [data, setData] = useState()
-  // const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [location, setLocation] = useState()
 
   
   useEffect(() => {
-    console.log('useeffect')
-    const coordinates = () => {
-     navigator.geolocation.getCurrentPosition(position => {
+    navigator.geolocation.getCurrentPosition(position => {
       setLocation(position.coords)
     })
-    fetchData()
-  }
 
   }, [])
   
   
+  useEffect(() => {
+    const fetchData = async () => {
+
+      
+   //  make this function be invoked by timer?
+  
+      const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=2cef8d9b64447d4ce805fcfbe539ea4b&units=imperial`) 
+      const fetchedData = await res.json()
+      console.log('got data')
+      setData(fetchedData)
+      setLoading(false)
+    }
+    
+    fetchData()
+  }, [location])
   
 
-  const fetchData = async () => {
-    console.log('entered fetch')
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=2cef8d9b64447d4ce805fcfbe539ea4b&units=imperial`) 
-    const fetchedData = await res.json()
-    console.log('got data')
-    setData(fetchedData)
-    // setLoading(false)
-  }
+  
 
   console.log( data ? data : 'no data')
   // console.log(currentDate.getDate(), currentDate.getHours() + 
@@ -38,11 +42,12 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <div className="container">
+        <h1>hi</h1>
      { data &&
-        <>
+        <> 
         <p>{`It is currently ${data.main.temp} in ${data.name}`}</p>
         <p className="date">todays date</p>
-       </> 
+      </> 
     }
         
       </div>
